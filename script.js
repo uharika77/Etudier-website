258208e0adca7037f5bc21b3171f7597f69019fe
uharika77 <117594183+uharika77@users.noreply.github.com>
2024-06-15 18:15:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "60%";
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.display = "none";
+}
